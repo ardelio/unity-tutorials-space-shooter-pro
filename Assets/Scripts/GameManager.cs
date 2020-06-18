7d163aabc5a4e50e4dc85dc787f262a303117f01
Anthony Sceresini <asceresini@gmail.com>
2020-06-19 00:00:08 +1000

    Add MainMenu and restart game after death

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private bool _isGameOver = false;
+
+    void Update()
+    {
+        bool restartKeyIsPressed = Input.GetKey(KeyCode.R);
+
+        if (_isGameOver && restartKeyIsPressed)
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.buildIndex);
+        }
+    }
+
+    public void GameOver()
+    {
+        _isGameOver = true;
+    }
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -10,13 +10,32 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private Text _gameOverText = null;
     [SerializeField]
+    private Text _restartText = null;
+    [SerializeField]
     private Image _livesImg = null;
     [SerializeField]
     private Sprite[] _livesSprites = null;
 
+    private GameManager _gameManager = null;
+
     private void Start()
     {
         _gameOverText.gameObject.SetActive(false);
+        _restartText.gameObject.SetActive(false);
+        
+        GameObject gameManagerGameObject = GameObject.Find("GameManager");
+
+        if (gameManagerGameObject == null)
+        {
+            throw new System.ArgumentNullException("[UIManager] GameManager game object not found");
+        }
+
+        _gameManager = gameManagerGameObject.GetComponent<GameManager>();
+
+        if (_gameManager == null)
+        {
+            throw new System.ArgumentNullException("[UIManager] GameManager component not found");
+        }
     }
 
     public void UpdateScore(int playerScore)
@@ -37,6 +56,8 @@ public class UIManager : MonoBehaviour
     private void GameOver()
     {
         StartCoroutine(GameOverTextFlickerRoutine());
+        _restartText.gameObject.SetActive(true);
+        _gameManager.GameOver();
     }
 
     private IEnumerator GameOverTextFlickerRoutine()
